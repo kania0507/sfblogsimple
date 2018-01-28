cbc35f6053ea958522a09241ad6d006f03cd2a7e
Ania <kania0507@gmail.com>
2018-01-28 16:24:56 +0100

    male porzadki w twig

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -31,8 +31,7 @@ class CategoryController extends Controller
 			$paginator = $this->get('knp_paginator');
 			$pagination = $paginator->paginate(
 			$qb, $request->query->get('page',1), 20);
-			
-		
+					
         return $this->render('category/index.html.twig', array('categories' => $pagination));
 	}
 	
@@ -41,43 +40,17 @@ class CategoryController extends Controller
 	* @Route("/categories/{id}", name="category_show")
 	*/
 	public function showAction($id)
-	{	
-		
+	{				
+		$repository = $this->getDoctrine()->getRepository(Category::class);
+		$category = $repository->find($id);
 	
-$repository = $this->getDoctrine()->getRepository(Category::class);
-
-$category = $repository->find($id);
-	
-/*
-		$category = $this->getDoctrine()
-		->getManager()
-        ->getRepository("AppBundle:Category")//"Category::class")
-        ->find($id);
-*/
-		/*
-		$category = $this->getDoctrine()
-				->getRepository('AppBundle:Category')
-				->findBy(array('id' => $category->getId()));
-*/
-		//$cat_repo=$this->getDoctrine()->getEntityManager()->getRepository("AppBundle:Category");
-		//$cat_repo=
-		//$category=$this->getDoctrine()->getRepository("AppBundle:Category")->find($category->id);
-		//$category=$cat_repo->find($id);
-		
-		//new \AppBundle\Entity\Category();
-		
-	
+			
 		if (!$category) {
 			throw $this->createNotFoundException(
 				'No category found for id '.$id
 			);
 		}
-			
-		
 
-		 return $this->render('category/show.html.twig', array('category'=>$category));
-		
-    
-	}
-	
+		 return $this->render('category/show.html.twig', array('category'=>$category));		    
+	}	
 }
